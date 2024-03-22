8fca72bfbab5e9e2df679c283577b102e15a6fa4
sharmashekhar0 <beingperfect76@gmail.com>
2024-03-22 23:32:48 +0530

    add frontend files

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="flex justify-center items-center h-screen bg-gray-900 text-white">
+			<h1 className="text-5xl">Frontend Under Development</h1>
+		</div>
+	);
+}
+
+export default App;
